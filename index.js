9044328a0474a284155ec97c879c2cd66f91f302
aseelflihan <aseelflihan@gmail.com>
2023-01-24 17:27:28 +0300

    complated assginment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#Red";
+
+dodger.style.bottom = "0px";
+dodger.style.left = "10px";
+
+const movment = (e) => {
+  if (e.key === "left") {
+    moveDodgerLeft(e);
+  }
+  if (e.key === "right") {
+    moveDodgerRight(e);
+  }
+};
+
+const moveDodgerLeft = (e) => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  dodger.style.left = `${left - 1}px`;
+};
+
+const moveDodgerRight = (e) => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+};
+document.addEventListener("key", movment);
